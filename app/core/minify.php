70a023e74b9704e7c34f2d88098aba0a16a9efc7
nestorpons <nestorpons@gmail.com>
2019-04-02 15:40:46 +0200

    modificado LessCompiler

--- app/core/minify.php
+++ app/core/minify.php
@@ -1,4 +1,11 @@
 <?php
+// Script para comprimir los ficheros css y js
+// Carpetas de trabajo /htdocs/css y /htdocs/js
+// Carpeta con archivos compilados /htdocs/build
+
 use MatthiasMullie\Minify;
-$minifier = new Minify\JS(\FOLDERS\JS . 'index.js');
-$minifier->minify(\FOLDERS\BUILD . 'index.min.js');
+$minifier_css = new Minify\CSS(\FOLDERS\CSS . 'main.css');
+$minifier_CSS->minify(\FOLDERS\BUILD . 'main.min.css');
+
+$minifier_JS = new Minify\JS(\FOLDERS\JS . 'index.js');
+$minifier_JS->minify(\FOLDERS\BUILD . 'index.min.js');
