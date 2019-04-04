354cb771971ba15fd550624cd9186cb11ead8a41
nestorpons <nestorpons@gmail.com>
2019-04-04 10:41:36 +0200

    Impplementado less

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,5 +5,6 @@ require_once dirname(__DIR__) . '/app/config/vars.php';
 require_once dirname(__DIR__) . '/app/config/app.php';
 require_once dirname(__DIR__) . '/vendor/autoload.php';  
 require_once \FOLDERS\CORE . 'minify.php';
+require_once \FOLDERS\CORE . 'less.php';
 
 new \app\core\Router($_REQUEST);
