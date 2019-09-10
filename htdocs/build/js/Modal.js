64dd289ba38bc813083ca1a7d2cd9bb205ed7d7e
nestorpons <nestorpons@gmail.com>
2019-09-10 09:58:13 +0200

    antes fusionar con admin1

---
+++ htdocs/build/js/Modal.js
@@ -0,0 +1,58 @@
+
+    class Modal{
+      constructor(id){
+
+        this.el = document.getElementById(id)
+        this.$el = $('#' + id)
+        this.$switch = $('#control_' + id)
+        this.$el.keyup(e => (e.key === 'Escape') && this.close())
+      }
+      clear(){
+        this.el.getElementsByTagName('form')[0].reset()
+        return this
+      }
+      setTile(tile){
+        this.el.getElementsByClassName('tile')[0].innerHTML = tile
+        return this
+      }
+      open(data = null){
+        if(data) this.load(data)
+        this.$switch.prop('checked',true)
+        let o = this.el.querySelector('[tabindex="1"]')
+        o != null && o.focus() 
+        return this
+      }
+      close(){
+        this.$switch.prop('checked',false)
+        return this
+      }
+      load(data){
+        const inputs = this.$el.find('input'),
+              selects = this.$el.find('select'),
+              d = exist(data.id) ? data : data[0]
+
+        selects.each(function(){
+
+          $(this).find('[selected]').attr('selected',false)
+          if(d.id != -1)
+            $(this)
+              .find('option[value="'+d[$(this).attr('name')]+'"]').attr('selected', true)    
+          else
+            $(this)
+              .find('[default]').attr('selected',true)
+        })
+        inputs.each(function(){
+          if($(this).attr('type') == 'checkbox' || $(this).attr('type') == 'radio'){
+            if(d[$(this).attr('name')] == $(this).val()) $(this).prop('checked', true)
+            else $(this).prop('checked', false)  
+
+          }
+          else $(this).val(d[$(this).attr('name')])
+        })
+        return this
+      }
+      getData(){ 
+        return app.formToObject(this.el)
+      }
+    } 
+  
---
+++ htdocs/build/js/Select.js
@@ -0,0 +1,56 @@
+
+    class Select extends Component{
+        constructor(id){
+            super()
+            this.el = document.getElementById(id).getElementsByTagName('select')[0]
+            this.o =  this.el.getElementsByTagName('option')
+            this.CLASS_SELECTED = 'valid'
+        }
+        addOption(value, option){
+            let line = document.createElement('option')
+                line.value = value 
+                line.innerHTML = option
+            this.el.append(line)
+        }
+        default(){
+            this.el.value = 0
+        }
+        clear(){
+            let ops = this.el.querySelector('option:not(:first-child)')
+            for(let i in ops){
+                if(ops[i]) this.el.remove(ops[i].selectedIndex)
+            }
+        }
+        reset(){
+
+            this.el.classList.remove(this.CLASS_SELECTED)
+            
+            let options = this.el.getElementsByTagName('option')
+
+            for (let option of options) {
+                option.selected = false 
+            }
+        }
+        value(v){
+            if(v != undefined){
+                this.el.value = v
+                this.el.seletedIndex = v
+            }
+            return  this.el.value
+        }
+        
+        option(data){
+            let options = this.el.getElementsByTagName('option'),
+                optionselected = false
+
+            for (let option of options) {
+                if(option.getAttribute('value') == data) {
+                    option.selected = true 
+                    optionselected = true
+                } else option.selected = false 
+            }
+
+            if(optionselected && data != 0 ) this.el.classList.add(this.CLASS_SELECTED)
+            else this.el.classList.remove(this.CLASS_SELECTED)
+        }
+    }
