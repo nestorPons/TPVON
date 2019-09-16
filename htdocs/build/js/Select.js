0e174e31224a57ce57b57be358ed203f5002d9c2
nestorpons <nestorpons@gmail.com>
2019-09-16 11:03:08 +0200

    Reparado select.clear

--- htdocs/build/js/Select.js
+++ htdocs/build/js/Select.js
@@ -4,7 +4,7 @@
             super()
             this.el = document.getElementById(id).getElementsByTagName('select')[0]
             this.o =  this.el.getElementsByTagName('option')
-            this.CLASS_SELECTED = 'valid'
+            this.CLASS_SELECTED = 'valid' // Constante del estado seleccionado del select
         }
         addOption(value, option){
             let line = document.createElement('option')
@@ -16,13 +16,12 @@
             this.el.value = 0
         }
         clear(){
-            let ops = this.el.querySelector('option:not(:first-child)')
-            for(let i in ops){
-                if(ops[i]) this.el.remove(ops[i].selectedIndex)
+            let ops = this.el.querySelectorAll('option:not(:first-child)')
+            for(let o of ops){
+                if(o) this.el.removeChild(o)
             }
         }
         reset(){
-
             this.el.classList.remove(this.CLASS_SELECTED)
             
             let options = this.el.getElementsByTagName('option')
