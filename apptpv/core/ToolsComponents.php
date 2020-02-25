75bff82c3d72e42533275922d15c638880b58b82
nestorpons <nestorpons@gmail.com>
2020-02-25 15:01:50 +0100

    bugs - componentes extendidos

--- apptpv/core/ToolsComponents.php
+++ apptpv/core/ToolsComponents.php
@@ -34,9 +34,10 @@ trait ToolsComponents
     {
         if (!isset($this->str_components))  $this->search_exist_components();
         // Buscar el primer componente
+
         if (
             preg_match(
-                "/(<({$this->str_components})(\s[^>\/]*)?(>|\/>)?)(.*)/si",
+                "/(<({$this->str_components})[^-_](\s[^>\/]*)?(>|\/>)?)(.*)/si",
                 $text_process,
                 $matches
             )
@@ -47,6 +48,7 @@ trait ToolsComponents
             $name_component = $matches[2];
             $simple_tag = $matches[4] == '/>';
             $text_process = $matches[5];
+            prs($matches);
 
             if ($simple_tag) {
                 $this->found_components[] = new Tag($tag_code);
@@ -71,7 +73,7 @@ trait ToolsComponents
                         } else {
                             if ($nested == 0) {
                                 // Tenemos el componente completo
-                                $rest = substr($text_process, 0, $pos); 
+                                $rest = substr($text_process, 0, $pos);
                                 $element = $tag_code . $rest;
                                 $this->found_components[] = new Tag($element);
                                 $text_process = \str_replace($rest, '', $text_process);
